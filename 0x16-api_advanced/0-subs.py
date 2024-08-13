e121702a1f2bb60b9d8146abf6d4e4762bee99ad
Gildo Chauze <gildochauze@gmail.com>
2024-08-13 19:51:08 +0200

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Method to query the number of subscriber"""
+
+
+def number_of_subscribers(subreddit):
+    """Retrieves the number of subscriber on the subreddit"""
+    import requests
+
+    subs = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        allow_redirects=False,
+    )
+
+    if subs.status_code >= 300:
+        return 0
+
+    return subs.json().get("data").get("subscribers")
