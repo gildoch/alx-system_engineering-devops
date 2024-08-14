ea6901410c6e5d556ba56a0648436ba8ad0be438
Gildo Chauze <gildochauze@gmail.com>
2024-08-14 09:04:01 +0200

    Fix wrong output 0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """Method to query the number of subscriber"""
 
+import requests
+
 
 def number_of_subscribers(subreddit):
     """Retrieves the number of subscriber on the subreddit"""
-    import requests
 
-    subs = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False,
-    )
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyRedditBot/0.1 (by u/GildoChauze; \
+               learning purposes)"}
 
-    if subs.status_code >= 300:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data["data"]["subscribers"]
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-
-    return subs.json().get("data").get("subscribers")
