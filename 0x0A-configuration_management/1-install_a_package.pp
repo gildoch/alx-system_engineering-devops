8230d194343bfef8faee47d6894b29f3ef09c2d9
Gildo Chauze <gildochauze@gmail.com>
2024-08-19 16:30:46 +0200

    Add pip lib install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
-# install flask using Puppet
-exec { 'flask':
-  command =>'/usr/bin/apt-get -y install flask -v 2.1.0'
+# Ensure that Python 3 and pip3 are installed
+package { ['python3', 'python3-pip']:
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
